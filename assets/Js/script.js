7a8bb83cacade5c4c4cd7009405ce7c1cb16fbc4
Klaus Cedillo <klausmcedillo@gmail.com>
2022-04-05 01:00:44 -0500

    Display Next Question

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -1,8 +1,52 @@
+
+// Document Selectors
 var startButton = document.querySelector(".start");
 var timer = document.querySelector(".clock");
+var menuSection = document.querySelector(".startGame");
+var questionSection = document.querySelector(".questions");
 
+
+var questionAsked = document.querySelector(".question");
+var listA = document.querySelector(".optionA");
+var listB = document.querySelector(".optionB");
+var listC = document.querySelector(".optionC");
+var listD = document.querySelector(".optionD");
+
+// Variables
 var seconds = 0;
 var minutes = 5;
+var score = 0;
+var currentQuestion = 0;
+
+// Objects with Questions
+let question0 = {
+    question: 'How much is 2+2?',
+    optionA: 'a) 1',
+    optionB: 'b) 2',
+    optionC: 'c) 3',
+    optionD: 'd) 4',
+    answer: 'd',
+};
+
+let question1 = {
+    question: 'How much is 2+1?',
+    optionA: 'a) 1',
+    optionB: 'b) 2',
+    optionC: 'c) 3',
+    optionD: 'd) 4',
+    answer: 'c',
+};
+
+let question2 = {
+    question: 'How much is 1+1?',
+    optionA: 'a) 1',
+    optionB: 'b) 2',
+    optionC: 'c) 3',
+    optionD: 'd) 4',
+    answer: 'b',
+};
+
+let questions = [question0,question1,question2]
 
 function displayTimer() {
     if (seconds < 10) {
@@ -35,6 +79,24 @@ function updateTimer(){
 function startTimer(event) {
     event.preventDefault();
     setInterval(updateTimer, 1000);
+    menuSection.style.display = "none";
+    questionSection.style.display = "flex";
+
 }
 
+function nextQuestion() {
+    next = questions[currentQuestion]
+    
+    questionAsked.textContent = next.question;
+    listA.textContent = next.optionA;
+    listB.textContent = next.optionB;
+    listC.textContent = next.optionC;
+    listD.textContent = next.optionD;
+
+    currentQuestion++;
+}
+
+nextQuestion();
 startButton.addEventListener("click", startTimer);
+
+
